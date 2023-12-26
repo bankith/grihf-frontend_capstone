5e49ed1ab2da7d2d5bd4b04634a0f03ac4512457
name <MY_NAME@example.com>
2023-12-26 12:45:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import Notification from './Components/Notification/Notification';
 function App() {
 
   return (
     <div className="App">
         <BrowserRouter>
-          <Navbar/>
+          <Notification>
               <Routes>
-              <Route path="/" element={<Landing_Page/>}/>
-              <Route path="/instant-consultation" element={<InstantConsultation />} />
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<SignUp />}/>
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
+                 <Route path="<component_route>" element={<component_name/>}/> //replace the component_route with the component path  and component_name with the component name as imported in the **App.js file**. 
+                />
               </Routes>
-            
+          </Notification>
         </BrowserRouter>
-       
     </div>
   );
 }
