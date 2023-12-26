5692bf62dd4c327a71e1eff7261eba7c4bfb9df6
name <MY_NAME@example.com>
2023-12-26 12:50:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
                 <Route path="/login" element={<Login />}/>
                 <Route path="/signup" element={<SignUp />}/>
                 <Route path="/instant-consultation" element={<InstantConsultation />} />
-                 <Route path="<component_route>" element={<component_name/>}/> //replace the component_route with the component path  and component_name with the component name as imported in the **App.js file**. 
-                />
+                 
               </Routes>
           </Notification>
         </BrowserRouter>
